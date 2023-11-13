f06d34759282fb5ad671f43ef94af41a8e18df7f
bhargavnaidu77 <120448254+bhargavnaidu77@users.noreply.github.com>
2023-11-13 14:13:40 +0530

    Update chatRoutes.js

--- chatRoutes.js
+++ chatRoutes.js
@@ -1,4 +1,3 @@
-// chatRoutes.js
 const express = require("express");
 const router = express.Router();
 const middleware = require("./middleware");
@@ -21,24 +20,32 @@ router.post("/sendMessage", async (req, res) => {
 
         await newMessage.save();
 
-        res.status(200).json({ message: "Message sent successfully" });
+        res.status(200).json({ message: "Message sent successfully", isSender: true });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Server Error" });
     }
 });
-
 
 // Example chat route to get all messages
 router.get("/getMessages", async (req, res) => {
     try {
+        const userId = req.user.id; // User ID from the authentication middleware
+
         // Retrieve all messages and populate the associated user details
         const messages = await ChatMessage.find().populate("user", "username");
 
-        res.status(200).json({ messages });
+        // Update messages to include the isSender property
+        const updatedMessages = messages.map((msg) => ({
+            ...msg._doc,
+            isSender: msg.user._id.toString() === userId.toString(),
+        }));
+        res.status(200).json({ messages: updatedMessages });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Server Error" });
     }
 });
+
+
 module.exports = router;
